ff3190d3e1594d828e7a9e5a8fe17bf6ae94bad8
Julia <juliavved@gmail.com>
2025-11-29 12:45:45 -0500

    Changed debug mode

--- app2.py
+++ app2.py
@@ -560,6 +560,8 @@ def clear_memory(user_id):
 
 
 if __name__ == '__main__':
+    import os
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
